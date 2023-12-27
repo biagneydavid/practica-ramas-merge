84c98cf43861963edcc3cb5b93ac0500a3140aa7
davicocode <154987897+davicocode@users.noreply.github.com>
2023-12-27 10:37:20 -0300

    Otra forma de eliminar el ultimo caracter

--- script.js
+++ script.js
@@ -14,5 +14,6 @@ function limpiarPantalla() {
 
 function deshacer1() {
     const valorPantalla = document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
+    const resultado = valorPantalla.substring(0, valorPantalla.lenght -1);
+    document.getElementById('pantalla').value = resultado;
 }
